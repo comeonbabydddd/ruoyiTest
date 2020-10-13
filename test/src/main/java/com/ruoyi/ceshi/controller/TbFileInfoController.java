d93f4330143f7ed27a3b8d620ccb4e26deba696d
hjx <2401791547@qq.com>
2020-10-13 17:26:07 +0800

    文件上传

--- test/src/main/java/com/ruoyi/ceshi/controller/TbFileInfoController.java
+++ test/src/main/java/com/ruoyi/ceshi/controller/TbFileInfoController.java
@@ -1,15 +1,15 @@
 package com.ruoyi.ceshi.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.ruoyi.common.fastdfs.FastDFSClientWrapper;
+import com.ruoyi.common.fastdfs.FileInfo;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.ceshi.domain.TbFileInfo;
@@ -18,6 +18,8 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 /**
  * 文件Controller
@@ -33,7 +35,8 @@ public class TbFileInfoController extends BaseController
 
     @Autowired
     private ITbFileInfoService tbFileInfoService;
-
+    @Autowired
+    private FastDFSClientWrapper fastDFSClientWrapper;
     @RequiresPermissions("ceshi:fileInfo:view")
     @GetMapping()
     public String fileInfo()
@@ -67,7 +70,35 @@ public class TbFileInfoController extends BaseController
         ExcelUtil<TbFileInfo> util = new ExcelUtil<TbFileInfo>(TbFileInfo.class);
         return util.exportExcel(list, "fileInfo");
     }
+    /**
+     * 新增上传文件弹出层
+     */
+    @GetMapping("/addFileTable")
+    public String addFileTable()
+    {
+        return prefix + "/addFileTable";
+    }
+    /**
+     * 新增保存文件
+     */
+    @RequiresPermissions("ceshi:fileInfo:addFileTableSave")
+    @Log(title = "文件", businessType = BusinessType.INSERT)
+    @PostMapping("/addFileTableSave")
+    @ResponseBody
+    public AjaxResult addFileTableSave(@RequestParam("file") MultipartFile multipartFile) throws Exception {
 
+        try {
+            String uploadFile = fastDFSClientWrapper.uploadFile(multipartFile);
+            String name = multipartFile.getName();
+            TbFileInfo fileInfo = new TbFileInfo();
+            fileInfo.setFileUrl(uploadFile);
+            fileInfo.setFileName(name);
+            return AjaxResult.success(tbFileInfoService.insertTbFileInfo(fileInfo));
+        } catch (IOException e) {
+            e.printStackTrace();
+             throw new Exception(e);
+        }
+    }
     /**
      * 新增文件
      */
@@ -76,7 +107,6 @@ public class TbFileInfoController extends BaseController
     {
         return prefix + "/add";
     }
-
     /**
      * 新增保存文件
      */
