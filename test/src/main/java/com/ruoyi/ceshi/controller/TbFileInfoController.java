b4bc8d3dcd64d897fe4a7c4ca34e9b0284a7492c
hjx <2401791547@qq.com>
2020-10-14 11:34:34 +0800

    下载文件

--- test/src/main/java/com/ruoyi/ceshi/controller/TbFileInfoController.java
+++ test/src/main/java/com/ruoyi/ceshi/controller/TbFileInfoController.java
@@ -1,10 +1,13 @@
 package com.ruoyi.ceshi.controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import com.ruoyi.common.fastdfs.FastDFSClientWrapper;
 import com.ruoyi.common.fastdfs.FileInfo;
+import com.ruoyi.common.utils.file.FileUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,26 +24,28 @@ import com.ruoyi.common.core.page.TableDataInfo;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 文件Controller
- * 
+ *
  * @author ruoyi
  * @date 2020-10-12
  */
 @Controller
 @RequestMapping("/ceshi/fileInfo")
-public class TbFileInfoController extends BaseController
-{
+public class TbFileInfoController extends BaseController {
     private String prefix = "ceshi/fileInfo";
 
     @Autowired
     private ITbFileInfoService tbFileInfoService;
     @Autowired
     private FastDFSClientWrapper fastDFSClientWrapper;
+
     @RequiresPermissions("ceshi:fileInfo:view")
     @GetMapping()
-    public String fileInfo()
-    {
+    public String fileInfo() {
         return prefix + "/fileInfo";
     }
 
@@ -50,8 +55,7 @@ public class TbFileInfoController extends BaseController
     @RequiresPermissions("ceshi:fileInfo:list")
     @PostMapping("/list")
     @ResponseBody
-    public TableDataInfo list(TbFileInfo tbFileInfo)
-    {
+    public TableDataInfo list(TbFileInfo tbFileInfo) {
         startPage();
         List<TbFileInfo> list = tbFileInfoService.selectTbFileInfoList(tbFileInfo);
         return getDataTable(list);
@@ -64,20 +68,20 @@ public class TbFileInfoController extends BaseController
     @Log(title = "文件", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
     @ResponseBody
-    public AjaxResult export(TbFileInfo tbFileInfo)
-    {
+    public AjaxResult export(TbFileInfo tbFileInfo) {
         List<TbFileInfo> list = tbFileInfoService.selectTbFileInfoList(tbFileInfo);
         ExcelUtil<TbFileInfo> util = new ExcelUtil<TbFileInfo>(TbFileInfo.class);
         return util.exportExcel(list, "fileInfo");
     }
+
     /**
      * 新增上传文件弹出层
      */
     @GetMapping("/addFileTable")
-    public String addFileTable()
-    {
+    public String addFileTable() {
         return prefix + "/addFileTable";
     }
+
     /**
      * 新增保存文件
      */
@@ -96,17 +100,40 @@ public class TbFileInfoController extends BaseController
             return AjaxResult.success(tbFileInfoService.insertTbFileInfo(fileInfo));
         } catch (IOException e) {
             e.printStackTrace();
-             throw new Exception(e);
+            throw new Exception(e);
         }
     }
+
+    /**
+     * 下载文件
+     */
+    @RequiresPermissions("ceshi:fileInfo:downLoadFile")
+    @Log(title = "文件", businessType = BusinessType.OTHER)
+    @GetMapping("/downLoadFile")
+    @ResponseBody
+    public InputStream downLoadFile(@RequestParam("id") Long id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+            TbFileInfo tbFileInfo = tbFileInfoService.selectTbFileInfoById(id);
+            FileInfo fileInfo = new FileInfo();
+            fileInfo.setName(tbFileInfo.getFileName());
+            fileInfo.setFileUrl(tbFileInfo.getFileUrl());
+            byte[] bytes = fastDFSClientWrapper.downLoadFile(fileInfo);
+//            response.setCharacterEncoding("utf-8");
+//            response.setContentType("multipart/form-data");
+//            response.setHeader("Content-Disposition",
+//                    "attachment;fileName=" + FileUtils.setFileDownloadHeader(request, tbFileInfo.getFileName()));
+//            FileUtils.writeBytes(bytes, response.getOutputStream());
+            return new ByteArrayInputStream(bytes);
+    }
+
+
     /**
      * 新增文件
      */
     @GetMapping("/add")
-    public String add()
-    {
+    public String add() {
         return prefix + "/add";
     }
+
     /**
      * 新增保存文件
      */
@@ -114,8 +141,7 @@ public class TbFileInfoController extends BaseController
     @Log(title = "文件", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(TbFileInfo tbFileInfo)
-    {
+    public AjaxResult addSave(TbFileInfo tbFileInfo) {
         return toAjax(tbFileInfoService.insertTbFileInfo(tbFileInfo));
     }
 
@@ -123,8 +149,7 @@ public class TbFileInfoController extends BaseController
      * 修改文件
      */
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable("id") Long id, ModelMap mmap)
-    {
+    public String edit(@PathVariable("id") Long id, ModelMap mmap) {
         TbFileInfo tbFileInfo = tbFileInfoService.selectTbFileInfoById(id);
         mmap.put("tbFileInfo", tbFileInfo);
         return prefix + "/edit";
@@ -137,8 +162,7 @@ public class TbFileInfoController extends BaseController
     @Log(title = "文件", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(TbFileInfo tbFileInfo)
-    {
+    public AjaxResult editSave(TbFileInfo tbFileInfo) {
         return toAjax(tbFileInfoService.updateTbFileInfo(tbFileInfo));
     }
 
@@ -147,10 +171,9 @@ public class TbFileInfoController extends BaseController
      */
     @RequiresPermissions("ceshi:fileInfo:remove")
     @Log(title = "文件", businessType = BusinessType.DELETE)
-    @PostMapping( "/remove")
+    @PostMapping("/remove")
     @ResponseBody
-    public AjaxResult remove(String ids)
-    {
+    public AjaxResult remove(String ids) {
         return toAjax(tbFileInfoService.deleteTbFileInfoByIds(ids));
     }
 }
